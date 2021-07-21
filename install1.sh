9fc1cf09f34a64ad818de310f3833ce9d56cc232
Michiel R <demichiel@gmail.com>
2021-07-21 14:37:33 +0200

    Add .NET SDK, update node

--- install1.sh
+++ install1.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-echo "Installing programs"
-curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-sudo apt-get install -y zsh vim curl wget git unzip fonts-powerline mosh nodejs locate fonts-firacode
+echo "Installing programs and Jetbrains PPA"
+wget https://packages.microsoft.com/config/ubuntu/21.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+curl -s https://s3.eu-central-1.amazonaws.com/jetbrains-ppa/0xA6E8698A.pub.asc | sudo apt-key add -
+echo "deb http://jetbrains-ppa.s3-website.eu-central-1.amazonaws.com focal main" | sudo tee /etc/apt/sources.list.d/jetbrains-ppa.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y apt-transport-https zsh vim curl wget git unzip fonts-powerline mosh nodejs locate fonts-firacode dotnet-sdk-5.0
 echo "installed node npm zsh vim curl wget git unzip fonts-powerline mosh locate fonts-firacode grc"
 
 echo "set git config"
